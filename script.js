666fde53fbedb6babe51b3a925e0d1f52235c32c
Brandon Wong <b.m.wong77@gmail.com>
2021-02-13 12:27:31 -0500

    updated api info onto DOM

--- script.js
+++ script.js
@@ -9,17 +9,31 @@ animeApp.getAnime = function(anime) {
         dataType: `json`,
     })
     .then(function(res) {
-        // animeApp.displayAnime(res);
-        console.log(res);
+        animeApp.displayAnime(res.results);
+        console.log(res.results);
     })
 }
 
 animeApp.searchAnimeTitle = function() {
-    $(`.searchBar`).on(`click`, function(event){
+    $(`.searchBar`).on(`submit`, function(event){
         event.preventDefault();
         const searchedAnimeTitle = $(`#search`).val();
         animeApp.getAnime(searchedAnimeTitle);
     })
+}
+
+animeApp.displayAnime = function(animeArrayinfo) {
+    animeArrayinfo.forEach(function(animeData){
+        console.log(animeData);
+        let animeContent = 
+            `<ul>
+                <li>
+                    <h2>${animeData.title}</h2>
+                    <img src="${animeData.image_url}" alt="${animeData.title}">
+                </li>
+            </ul> `;
+        $(`.searchedAnimeContent`).append(animeContent);
+    });
 }
 
 
